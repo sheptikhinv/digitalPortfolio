336841debd2f1287ba4415511a809b95529bc1ae
shept <sheptikhinv@gmail.com>
2024-05-04 13:38:48 +0500

    changed launch

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import uvicorn
 
 from database import db, models
+from helpers.config import get_value
 
 if __name__ == '__main__':
     db.connect()
     db.create_tables([models.User, models.Profile, models.Project, models.Comment, models.ProjectImage, models.Like])
     db.close()
-    uvicorn.run("routers.main:app", reload=True)
+    if get_value("is_development").lower() in ["true", "yes", "y"]:
+        uvicorn.run("routers.main:app", reload=True)
+    else:
+        uvicorn.run("routers.main:app", host="127.0.0.1", port=5000)
